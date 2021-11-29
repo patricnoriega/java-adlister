c1f477a23c031c1be4d007c1553c365e715a8221
patric noriega <patricnoriega@gmail.com>
2021-11-29 15:17:26 -0600

    feat: hello world

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,29 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
+        res.setContentType("text/html");
+        String nameQuery = req.getParameter("name");
+        String searchQuery = req.getParameter("q");
+        PrintWriter out = res.getWriter();
+
+        if (nameQuery != null) {
+            out.println("<h1>Hello, " + nameQuery + "!</h1>");
+        }
+        if (nameQuery == null) {
+            out.println("<h1>Hello, World!</h1>");
+        }
+        if (searchQuery != null) {
+            out.println("<p>Here are your results for " + searchQuery + "</p>");
+        }
     }
 }
+
